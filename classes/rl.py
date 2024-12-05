e3ad4436d960391b8ff6d1627fa489053906e7e4
Gerard Morales Riera <gerard.morales@estudiantat.upc.edu>
2024-12-05 16:04:12 -0600

    Update train()

--- classes/rl.py
+++ classes/rl.py
@@ -3,6 +3,7 @@ import torchviz
 import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
+from torchrl.data import ReplayBuffer
 
 import sys
 import os
@@ -28,17 +29,74 @@ class Actor(nn.Module):
     def forward(self, states, actions):
         return self.model(states, actions)
 
+class Critic(nn.Module):
+    """
+    Class to represent a Critic in the context of the SAC algorithm. Children class of nn.Module.
+    """
+    def __init__(self, in_dim: int, out_dim: int, hidden_dim: tuple[int]=[256, 64, 256, 64], lr: float=1e-3):
+        super(Critic, self).__init__()
+        self.role_type = "Critic"
+        self.in_dim = in_dim
+        self.out_dim = out_dim
+        self.hidden_dim = hidden_dim
+        self.lr = lr
+
+        layers = []
+        
+        for i in range(len(hidden_dim)):
+            if i == 0:
+                layers.append(nn.Linear(in_dim, hidden_dim[i]))
+            else:
+                layers.append(nn.Linear(hidden_dim[i-1], hidden_dim[i]))
+            layers.append(nn.ReLU())
+
+        layers.append(nn.Linear(hidden_dim[-1], out_dim))
+
+        self.sequential = nn.Sequential(*layers)
+
+        self.init_weights()
+
+    def init_weights(self):
+        initrange = 0.1
+        for module in self.sequential:
+            if type(module) == torch.nn.Linear:
+                    nn.init.zeros_(module.bias)
+                    nn.init.uniform_(module.weight, -initrange, initrange)
+
+    def forward(self, x):
+        return self.sequential(x)
+    
+class VNetwork(Critic):
+    """
+    Class to represent a V-network. Children class of Critic.
+    """
+    def __init__(self, state_dim, out_dim, hidden_dim: tuple[int]=[256, 64, 256, 64], lr: float=1e-3):
+        super(VNetwork, self).__init__(in_dim=state_dim, out_dim=out_dim, hidden_dim=hidden_dim, lr=lr)
+        self.critic_type = "V-network"
+
+    def forward(self, state):
+        # Fill the state with zeros if it is not the same size as the input dimension
+        if state.shape[0] < self.in_dim:
+            state = torch.cat([state, torch.zeros(self.in_dim - state.shape[0])])
+        elif state.shape[0] > self.in_dim:
+            state = state[-self.in_dim:]
+
+        x = state
+        x = super(VNetwork, self).forward(x)
+        return x
+
 class RL():
     """
     Class to represent the Soft Actor-Critic algorithm. Children class of nn.Module.
     """
     def __init__(self, conf: DataFromJSON, client: Client, save_path: str):
-        self.__role_type = "Reinforcement Learning"
+        self.__role_type = "Soft Actor-Critic"
         self.__conf = conf
         self.client = client
         self.save_path = save_path
         self.set_properties(conf)
-        self.losses = {"pi": []}
+        self.losses = {"v": [], "pi": []}
+        self.replay_buffer = ReplayBuffer()
         self.tensor_manager = TensorManager()
 
     def __str__(self) -> str:
@@ -57,21 +115,21 @@ class RL():
         Start the SAC algorithm.
         """
         # Create the agent and the critics
-        actor = self.create_entities()
+        actor, v= self.create_entities()
 
         # Warm up the agent
         list_states, list_actions = self.warm_up(actor)
 
         # Train the agent
-        actor = self.train(actor, list_states, list_actions)
+        actor, v = self.train(actor, v, list_states, list_actions)
 
         # Plot the losses
         self.plot_losses(self.losses)
 
         # Save the model
-        self.save_model(actor)
+        self.save_model(actor, v)
 
-    def create_entities(self) -> Actor:
+    def create_entities(self) -> tuple[Actor, VNetwork]:
         """
         Create the entities for the SAC algorithm.
         """
@@ -123,13 +181,17 @@ class RL():
 
         # Create the actor
         actor = Actor(model, lr=self.lambda_pi)
-        
+
+        # Create the NNs for the V-networks
+        v = VNetwork((self.state_dim + self.action_dim) * self.max_len, 1, lr=self.lambda_v)
+
         # Load the previous models if they exist
         if os.path.exists(self.save_path) and self.load_model and os.path.exists(f"{self.save_path}\\model.pth"):
             print("Loading previous models...")
             actor.model.load_state_dict(torch.load(f"{self.save_path}\\model.pth", weights_only=True))
+            v.load_state_dict(torch.load(f"{self.save_path}\\v.pth", weights_only=True))
 
-        return actor
+        return actor, v
 
     def warm_up(self, actor: Actor) -> tuple[list[torch.Tensor], list[torch.Tensor]]:
         """
@@ -171,7 +233,7 @@ class RL():
         print("Starting warm-up...")
 
         # Loop over all iterations
-        for _ in range(self.warm_up_steps):
+        for w in range(self.warm_up_steps):
             # Loop over all agents
             for idx, agt in enumerate(self.agents):
                 states = list_states[idx]
@@ -204,7 +266,7 @@ class RL():
                         "delta_time": self.time_increment
                     }
                     
-                    state, _, done = self.client.get_next_state("get_next", sending_data)
+                    state, reward, done = self.client.get_next_state("get_next", sending_data)
 
                     # Break if time is up
                     if done:
@@ -213,6 +275,9 @@ class RL():
 
                     # Normalize the state
                     vec_state = self.normalize_state(state)
+
+                    # Get the reward
+                    r = torch.FloatTensor([reward * self.reward_scale])
 
                     # Get the next state
                     s_next = torch.FloatTensor(vec_state)
@@ -228,6 +293,12 @@ class RL():
                     states = states[:, -self.max_len:, :]
                     actions = actions[:, -self.max_len:, :]
 
+                    # Augmented state for the next step
+                    aug_state_next = [states, actions]
+
+                    # Store in the buffer
+                    self.replay_buffer.add((aug_state, a, r, aug_state_next))
+
                     # Replace the states and actions lists
                     list_states[idx] = states
                     list_actions[idx] = actions
@@ -236,13 +307,14 @@ class RL():
         
         return list_states, list_actions
 
-    def train(self, actor: Actor, list_states: list[torch.Tensor], list_actions: list[torch.Tensor]):
+    def train(self, actor: Actor, v: VNetwork, list_states: list[torch.Tensor], list_actions: list[torch.Tensor]):
         """
         Begin the training of the SAC algorithm.
         """
         torch.autograd.set_detect_anomaly(True)
 
         # Optimizers
+        optimizer_v = optim.Adam(v.parameters(), lr=v.lr)
         optimizer_pi = optim.Adam(actor.model.parameters(), lr=actor.lr)
 
         # Loop flags
@@ -256,86 +328,161 @@ class RL():
             print(f"\nStarting iteration {iteration}...")
             iteration += 1
 
-            for idx, agt in enumerate(self.agents):
-                states = list_states[idx]
-                actions = list_actions[idx]
+            # Loop over all environment steps
+            for e in range(self.environment_steps):
+                # Loop over all agents
+                for idx, agt in enumerate(self.agents):
+                    states = list_states[idx]
+                    actions = list_actions[idx]
 
-                # Adjust the maximum length of the states and actions
-                states = states[:, -self.max_len:, :]
-                actions = actions[:, -self.max_len:, :]
+                    with torch.no_grad():
+                        # Adjust the maximum length of the states and actions
+                        states = states[:, -self.max_len:, :]
+                        actions = actions[:, -self.max_len:, :]
 
-                # Get the stochastic actions
-                stochastic_actions = actor(states, actions)
+                        # Create the augmented state
+                        aug_state = [states.clone(), actions.clone()]
 
-                # Select the last stochastic action
-                a_sto = stochastic_actions[-1, -1, :]
+                        # Get the stochastic actions
+                        stochastic_actions = actor(states, actions)
 
-                # Sample and convert the action
-                _, a = actor.model.reparametrization_trick(a_sto)
+                        # Select the last stochastic action
+                        a_sto = stochastic_actions[-1, -1, :]
 
-                # --------------- Environment's job to provide info ---------------
-                sending_data = {
-                    "agent_id": agt,
-                    "action": {
-                        "d_pitch": a[0].item() * 30,
-                        "d_roll": a[1].item() * 60
-                    },
-                    "delta_time": self.time_increment
-                }
-                
-                state, reward, done = self.client.get_next_state("get_next", sending_data)
+                        # Sample and convert the action
+                        _, a = actor.model.reparametrization_trick(a_sto)
+
+                        # --------------- Environment's job to provide info ---------------
+                        sending_data = {
+                            "agent_id": agt,
+                            "action": {
+                                "d_pitch": a[0].item() * 30,
+                                "d_roll": a[1].item() * 60
+                            },
+                            "delta_time": self.time_increment
+                        }
+                        
+                        state, reward, done = self.client.get_next_state("get_next", sending_data)
+
+                        # Break if time is up
+                        if done:
+                            print("Time is up!")
+                            break
+
+                        # Normalize the state
+                        vec_state = self.normalize_state(state)
+
+                        # Get the reward
+                        r = torch.FloatTensor([reward * self.reward_scale])
+
+                        # Get the next state
+                        s_next = torch.FloatTensor(vec_state)
+                        # --------------- Environment's job to provide info ---------------
+
+                        # Add it to the states
+                        states = torch.cat([states, s_next.unsqueeze(0).unsqueeze(0)], dim=1)
+
+                        # Add it to the actions
+                        actions = torch.cat([actions, a.unsqueeze(0).unsqueeze(0)], dim=1)
+
+                        # Adjust the maximum length of the states and actions
+                        states = states[:, -self.max_len:, :]
+                        actions = actions[:, -self.max_len:, :]
+
+                        # Augmented state for the next step
+                        aug_state_next = [states, actions]
+
+                        # Store in the buffer
+                        self.replay_buffer.add((aug_state, a, r, aug_state_next))
+
+                        # Replace the states and actions lists
+                        list_states[idx] = states
+                        list_actions[idx] = actions
 
                 # Break if time is up
                 if done:
-                    print("Time is up!")
                     break
 
-                # Normalize the state
-                vec_state = self.normalize_state(state)
+                if not e == 0:
+                    sys.stdout.write("\033[F")
+                print(f"Environment step {e+1}/{self.environment_steps} done!")
+            
+            # Break if time is up
+            if done:
+                break
 
-                # Get the reward
-                r = torch.FloatTensor([reward * self.reward_scale])
+            # Loop over all gradient steps
+            for g in range(self.gradient_steps):
+                with torch.no_grad():
+                    aug_state, a, r, aug_state_next = self.tensor_manager.full_squeeze(*self.replay_buffer.sample(1))
 
-                # Get the next state
-                s_next = torch.FloatTensor(vec_state)
-                # --------------- Environment's job to provide info ---------------
+                # Batchify the tensors neccessary for the transformer
+                aug_state, aug_state_next = self.tensor_manager.batchify(aug_state, aug_state_next)
 
-                # Add it to the states
-                states = torch.cat([states, s_next.unsqueeze(0).unsqueeze(0)], dim=1)
+                # Get the stochastic actions again
+                new_stochastic_actions = actor(aug_state[0], aug_state[1])
 
-                # Add it to the actions
-                actions = torch.cat([actions, a.unsqueeze(0).unsqueeze(0)], dim=1)
+                # Select the last stochastic action
+                a_new_sto = new_stochastic_actions[-1, -1, :]
 
-                # Adjust the maximum length of the states and actions
-                states = states[:, -self.max_len:, :]
-                actions = actions[:, -self.max_len:, :]
+                # Sample and convert the action
+                a_new_pretanh, a_new = actor.model.reparametrization_trick(a_new_sto)
 
-                # Replace the states and actions lists
-                list_states[idx] = states
-                list_actions[idx] = actions
+                # Reshape the augmented state to input to the Q-networks and the V-network as a single 1D tensor
+                aug_state_1D = torch.cat([aug_state[0], aug_state[1]], dim=2).view(-1)
+
+                # Log probability
+                sum = torch.tensor(0.0, dtype=torch.float32, requires_grad=True)
+                corrective_term = torch.tensor(0.0, dtype=torch.float32, requires_grad=True)
+
+                for i, feature in enumerate(a_new_sto):
+                    mean = feature[0]
+                    log_std = feature[1]
+                    std = torch.exp(log_std)
+                    var = std**2
+                    
+                    sum = sum + (a_new_pretanh[i] - mean)**2 / var # (x - mean)^2 / var
+                    sum = sum + 2 * log_std + torch.log(torch.tensor(2 * torch.pi, requires_grad=True)) # log(2 * pi * var) = 2 * log(std) + log(2 * pi)
+                    corrective_term = corrective_term - torch.log(1 - a_new[i]**2 + 1e-6) # -log(1 - tanh^2(a_new_pretanh)) (with epsilon to avoid division by zero)
+
+                log_prob = -0.5 * sum + corrective_term # transformation-corrected log probability
+
+                # ------------------------------------- CLARIFICATION -------------------------------------
+                # Each loss is 0.5 * (prediction - target)^2 = 0.5 * MSE(prediction, target)
+                # It is not the same the target VALUE of v (in a certain step) and the target NETWORK of v
+                # -----------------------------------------------------------------------------------------
+
+                # Target value for each loss
+                with torch.no_grad():
+                    target_v = r
 
                 # Set the gradients to zero
+                optimizer_v.zero_grad()
                 optimizer_pi.zero_grad()
 
                 # Compute the losses
-                J_pi = r
+                J_v = 0.5 * F.mse_loss(v(aug_state_1D), target_v)
+                J_pi = -log_prob * (r - v(aug_state_1D))
 
                 # Store the losses
+                self.losses["v"].append(J_v.item())
                 self.losses["pi"].append(J_pi.item())
 
                 # Backpropagate
+                J_v.backward()
                 J_pi.backward(retain_graph=True)
 
                 # Optimize parameters
+                optimizer_v.step()
                 optimizer_pi.step()
 
-                print("✔ Iteration done!")
+                if not g == 0:
+                    sys.stdout.write("\033[F")
+                print(f"Gradient step {g+1}/{self.gradient_steps} done!")
 
-                # Break if time is up
-                if done:
-                    break
+            print("✔ Iteration done!")
 
-        return actor
+        return actor, v
     
     def normalize_state(self, state: dict) -> list:
         """
@@ -362,17 +509,22 @@ class RL():
         """
         Plot the losses.
         """
+        fig, ax = plt.subplots(1, 2, figsize=(10, 10))
+
+        smoothed_v = pd.DataFrame(losses["v"]).rolling(window=int(len(losses["v"])/10)).mean()
         smoothed_pi = pd.DataFrame(losses["pi"]).rolling(window=int(len(losses["pi"])/10)).mean()
 
-        plt.plot(smoothed_pi)
-        plt.xlabel("Step")
-        plt.ylabel("Reward")
-        plt.title("Rewards over time")
+        ax[0, 0].plot(smoothed_v)
+        ax[0, 0].set_title("V-network loss")
+
+        ax[0, 1].plot(smoothed_pi)
+        ax[0, 1].set_title("Policy loss")
 
         plt.savefig(f"{self.save_path}\\losses.png", dpi=500)
     
-    def save_model(self, actor: Actor):
+    def save_model(self, actor: Actor, v: VNetwork):
         """
         Save the model to the specified path.
         """
         torch.save(actor.model.state_dict(), f"{self.save_path}\\model.pth")
+        torch.save(v.state_dict(), f"{self.save_path}\\v.pth")
